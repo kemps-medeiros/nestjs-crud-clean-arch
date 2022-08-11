f27c0900092b2328d84ec6a28505de2e1f68d44e
Kemps Medeiros <kempsmm@gmail.com>
2022-08-11 19:09:57 -0300

    feat: adding propeties to student entity

--- src/business/domain/student/entity/student.entity.ts
+++ src/business/domain/student/entity/student.entity.ts
@@ -1,1 +1,25 @@
-export default class Student {}
+import Identifier from '../value-object/identifier.value-object';
+import Name from '../value-object/name.value-object';
+import PhoneNumber from '../value-object/phone-number.value-object';
+import Registration from '../value-object/registration.value-objects';
+
+export type TCreateStudentPayload = {
+  id: string;
+  name: string;
+  registration: number;
+  phoneNumber: string;
+};
+
+export default class Student {
+  private _id: Identifier;
+  private _name: Name;
+  private _registration: Registration;
+  private _phoneNumber: PhoneNumber;
+
+  constructor(payload: TCreateStudentPayload) {
+    this._id = new Identifier(payload.id);
+    this._name = new Name(payload.name);
+    this._registration = new Registration(payload.registration);
+    this._phoneNumber = new PhoneNumber(payload.phoneNumber);
+  }
+}
