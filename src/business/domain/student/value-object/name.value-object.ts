0c1e7db8f4205e2f3ce365f520d243f0d6f997d6
Kemps Medeiros <kempsmm@gmail.com>
2022-08-12 15:04:48 -0300

    feat: adding validation function

--- src/business/domain/student/value-object/name.value-object.ts
+++ src/business/domain/student/value-object/name.value-object.ts
@@ -1,11 +1,13 @@
-import { IsDefined, Length } from 'class-validator';
+import { IsDefined, Length, validateSync } from 'class-validator';
+import { handleErrors } from '../../../common/error/handle-errors';
 
 export default class Name {
   @Length(2, 100)
-  @IsDefined()
   readonly value: string;
 
   constructor(value: string) {
     this.value = value;
+    const errors = validateSync(this);
+    handleErrors(errors);
   }
 }
