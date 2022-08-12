1f793929c92a51e3b5ac26d6fd1d34abbed79f7e
Kemps Medeiros <kempsmm@gmail.com>
2022-08-12 15:35:42 -0300

    feat: adding validantion function

--- src/business/domain/student/value-object/phone-number.value-object.ts
+++ src/business/domain/student/value-object/phone-number.value-object.ts
@@ -1,11 +1,13 @@
-import { IsDefined, IsPhoneNumber } from 'class-validator';
+import { IsPhoneNumber, validateSync } from 'class-validator';
+import { handleErrors } from '../../../common/error/handle-errors';
 
 export default class PhoneNumber {
-  @IsDefined()
-  @IsPhoneNumber()
+  @IsPhoneNumber('BR')
   readonly value: string;
 
   constructor(value: string) {
     this.value = value;
+    const errors = validateSync(this);
+    handleErrors(errors);
   }
 }
