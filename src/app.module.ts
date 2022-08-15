4f9dd7a25ddb31459e07022b72829845f39805bc
Kemps Medeiros <kempsmm@gmail.com>
2022-08-15 09:36:37 -0300

    feat: config database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'student-database',
+      entities: [
+        __dirname +
+          '../infrastructure/persistence/typeorm/**/*.entity{.ts,.js}',
+      ],
+      synchronize: true,
+    } as TypeOrmModuleOptions),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
