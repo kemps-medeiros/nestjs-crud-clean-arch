af562c914f7acaa99229c48e0c1acc254a7e12a6
Kemps Medeiros <kempsmm@gmail.com>
2022-08-15 17:59:36 -0300

    feat: implements getAll method

--- src/application/api/rest/controller/student/student.controller.ts
+++ src/application/api/rest/controller/student/student.controller.ts
@@ -1,6 +1,10 @@
-import { Body, Controller, Inject, Post } from '@nestjs/common';
-import { CreateStudentUseCaseToken } from 'src/application/dependecy-inversion/token/student.token';
+import { Body, Controller, Get, Inject, Post } from '@nestjs/common';
+import {
+  CreateStudentUseCaseToken,
+  ListStudentsUseCaseToken,
+} from 'src/application/dependecy-inversion/token/student.token';
 import ICreateStudentUseCase from 'src/business/domain/student/use-case/create-student.interface';
+import IListStudentsUseCase from 'src/business/domain/student/use-case/list-students.interface';
 import CreateStudentRequest from './request/create-student.request';
 
 @Controller('student')
@@ -8,10 +12,18 @@ export default class StudentController {
   constructor(
     @Inject(CreateStudentUseCaseToken)
     readonly createStudentUseCase: ICreateStudentUseCase,
+
+    @Inject(ListStudentsUseCaseToken)
+    readonly listStudentsUseCase: IListStudentsUseCase,
   ) {}
 
   @Post()
   async create(@Body() createStudentRequest: CreateStudentRequest) {
     return await this.createStudentUseCase.execute(createStudentRequest);
+  }
+
+  @Get()
+  async getAll() {
+    return await this.listStudentsUseCase.execute();
   }
 }
