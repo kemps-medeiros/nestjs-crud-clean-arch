68a537199853b7832dcc175515cd59572d05bfd5
Kemps Medeiros <kempsmm@gmail.com>
2022-08-19 11:47:36 -0300

    feat: implements edit method on controller

--- src/application/api/rest/controller/student/student.controller.ts
+++ src/application/api/rest/controller/student/student.controller.ts
@@ -1,13 +1,24 @@
-import { Body, Controller, Get, Inject, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Inject,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import {
   CreateStudentUseCaseToken,
   GetStudentByIdUseCaseToken,
   ListStudentsUseCaseToken,
+  UpdateStudentUseCaseToken,
 } from 'src/application/dependecy-inversion/token/student.token';
 import ICreateStudentUseCase from 'src/business/domain/student/use-case/create-student.interface';
 import IGetStudentByIdUseCase from 'src/business/domain/student/use-case/get-student-by-id.interface';
 import IListStudentsUseCase from 'src/business/domain/student/use-case/list-students.interface';
+import IUpdateStudentUseCase from 'src/business/domain/student/use-case/update-student.interface';
 import CreateStudentRequest from './request/create-student.request';
+import UpdateStudentRequest from './request/update-student.request';
 
 @Controller('student')
 export default class StudentController {
@@ -17,6 +28,9 @@ export default class StudentController {
 
     @Inject(ListStudentsUseCaseToken)
     readonly listStudentsUseCase: IListStudentsUseCase,
+
+    @Inject(UpdateStudentUseCaseToken)
+    readonly updateStudentUseCase: IUpdateStudentUseCase,
 
     @Inject(GetStudentByIdUseCaseToken)
     readonly getStudentByIdUseCase: IGetStudentByIdUseCase,
@@ -35,5 +49,11 @@ export default class StudentController {
   @Get(':id')
   async getStudentById(@Param('id') id: string) {
     return await this.getStudentByIdUseCase.execute(id);
+  }
+
+  @Put(':id')
+  async edit(@Param('id') id: string, @Body() request: UpdateStudentRequest) {
+    request.id = id;
+    return await this.updateStudentUseCase.execute(request);
   }
 }
