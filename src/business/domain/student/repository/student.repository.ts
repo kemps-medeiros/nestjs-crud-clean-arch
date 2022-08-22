4fa5c188471a46c41169543da22e9a2b7ab74563
Kemps Medeiros <kempsmm@gmail.com>
2022-08-22 09:42:57 -0300

    fix: fix create method

--- src/business/domain/student/repository/student.repository.ts
+++ src/business/domain/student/repository/student.repository.ts
@@ -3,12 +3,6 @@ import Student from '../entity/student.entity';
 
 export default abstract class StudentRepository {
   async create(student: Student): Promise<void> {
-    const existedStudent = await this.findById(student.id);
-
-    if (existedStudent) {
-      throw new Error('Student already exists on this database');
-    }
-
     await this.insert(student);
   }
 
